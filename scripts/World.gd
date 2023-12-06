c71b5150e501bdffa4108f81ea480c3f73cb60a4
Tanner Rabb <trabb31@gmail.com>
2023-12-06 18:36:23 -0500

    Cleaned up path generation Enemy will follow path after generation

--- scripts/World.gd
+++ scripts/World.gd
@@ -2,11 +2,18 @@ extends Node2D
 
 #Tilemap that contains my path
 @onready var tileMap = $Path
+#Path2D that is used to create a path for emeies to follow
+@onready var path2D = $EnemyDirections
+@onready var pathFollow2D = $EnemyDirections/PathFollow2D
+@onready var button = $Button
+
 #size of map - can measure 2d to get size in units
 #length is +1 to the actual size of map to make path look like it goes off the map
 @export var map_length:int = 41
 @export var map_height:int = 28
-var _path:Array[Vector2i]
+@export var speed = 100
+
+var _path:Array[Vector2i] = []
 
 var _pg:PathGenerator
 
@@ -14,7 +21,14 @@ func _ready():
 	_pg = PathGenerator.new(map_length, map_height)
 	_display_path()
 	_complete_grid()
-
+	_create_curve2d()
+	print(_pg.get_path())
+	
+	button.pressed.connect(self._button_pressed)
+	
+func _process(delta: float) -> void:
+	pathFollow2D.progress += speed * delta
+	
 func _display_path():
 	_path = _pg.generate_path()
 	
@@ -30,3 +44,23 @@ func _complete_grid():
 				#holds the current coords of the tile that will be filled with grass
 				var coords = Vector2i(x,y)
 				tileMap.set_cell(0, coords, 2, Vector2i(0,0), 0)
+	
+func _create_curve2d():
+	var _cleanX:int
+	var _cleanY:int
+	
+	path2D.curve.clear_points()
+	for coord in _pg.get_path():
+		# need to clean so the enemies are in the middle of the path
+		#coord value given by _path is the top left point of the box in the grid
+		if coord.x == 0:
+			_cleanX = coord.x
+		else:
+			_cleanX = (coord.x * 32) + 16
+			
+		_cleanY = (coord.y * 32) + 16
+		path2D.curve.add_point(Vector2i(_cleanX,_cleanY))
+		
+func _button_pressed():
+	print(get_tree().reload_current_scene())
+	print("Pressed!!")
--- scripts/path_generator.gd
+++ scripts/path_generator.gd
@@ -1,10 +1,13 @@
+#TO DO
+#give path a default distance it must go before picking a direction - feels pretty good right now
+# require the first path generation to be to the right, since it starts at x=-1 potential for long path off screen
 extends Object
 class_name PathGenerator
 
 var _grid_length:int
 var _grid_height:int
 var _path: Array[Vector2i]
-var _percentForDirectionChance:float
+var counter:int = 0
 
 var _rng = RandomNumberGenerator.new()
 
@@ -20,29 +23,37 @@ func generate_path():
 	#path start position
 	var x = -1
 	var y = _rng.randi_range(0,21)
-
+	
+	#reset counter
+	counter = 0
+	
 	#start making path
 	while x < _grid_length:
-		var counter:int
+		
 		if not _path.has(Vector2i(x,y)):
 			_path.append(Vector2i(x,y))
 		
 		var choice:int = randi_range(0,2)
 		
-		#path goes right
+		#for path right for the first tile
+		if counter == 0:
+			choice = 0
+			
+		#path goes right - x % 2 prevents creating turns with no gap
 		if choice == 0 or x % 2 == 0:
 			x += 1
-		#path goes up - can not pick this if there is a path ahead of it
-		elif choice == 1 and y < _grid_height and not _path.has(Vector2i(x,y+1)):
+		#path goes down - can not pick this if there is already a path down
+		elif choice == 1 and y < (_grid_height - 1) and not _path.has(Vector2i(x,y+1)):
 			y += 1
-		#path goes down - can not pick this if there is a path ahead of it
-		elif choice == 2 and y > 1 and not _path.has(Vector2i(x,y-1)):
+		#path goes up - can not pick this if there is already a path up
+		elif choice == 2 and y > 2 and not _path.has(Vector2i(x,y-1)):
 			y -= 1
+		#if it cant go 	
+		else:
+			x +=1
 		counter +=1
 	return _path
 	
 func get_path () -> Array[Vector2i]:
 	return _path
-	
-#TO DO
-#give path a default distance it must go before picking a direction - feels pretty good right now
+
